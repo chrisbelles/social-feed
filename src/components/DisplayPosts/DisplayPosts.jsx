3d496ce64f3dd7ce9cf8ed04521d13fc6a4dc2e3
chrisbelles <chris.belles1@gmail.com>
2023-02-15 22:55:03 -0600

    Trying to style the feedback buttons

--- src/components/DisplayPosts/DisplayPosts.jsx
+++ src/components/DisplayPosts/DisplayPosts.jsx
@@ -1,18 +1,18 @@
 import CustomDislike from "../CustomDislike/CustomDislike";
 import CustomLike from "../CustomLike/CustomLike";
-
+import "C:/Users/Chris/Desktop/devCodeCamp/social-feed-project2/src/stylesheet.css"
 
 const DisplayPosts = (props) => {
-    return ( 
-        <div className="post-wrapper">
-            <h1>{props.entry.name}</h1>
-            <p>{props.entry.comment}</p>
-            <div className="feedback-buttons">
-            <CustomLike /> 
-            <CustomDislike />
-            </div>
-        </div>    
-     );
+    return (
+      <div className="post-wrapper">
+        <h1>{props.entry.name}</h1>
+        <p>{props.entry.comment}</p>
+        <div className="feedback-buttons">
+          <CustomLike /> 
+          <CustomDislike />
+        </div>
+      </div>
+    );
 }
  
 export default DisplayPosts;
